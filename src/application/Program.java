ed14dbeab39c023e7a7c99d6601e922fe11189e4
Ulisses Batista Santana <santana.gru@gmail.com>
2024-02-14 19:26:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departmente;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Departmente obj = new Departmente(1, "maria");
-		System.out.println(obj);
+		Departmente obj = new Departmente(1, "Books");
+		
+		Seller seller = new Seller(2, "Maria", "maria@gmail.com", new Date(), obj);
+		
+		System.out.println(seller);
 	}
 
 }
